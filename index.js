94e6345faa85eb6775ca1427b5670329fbc23c81
Ana Rodriguez Reiner <arodriguezreiner@outlook.com>
2023-05-18 15:33:08 -0700

    commit lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+// Write your solution in this file
+let employee = {
+    name: "Ana",
+    streetAddress: "123 Lane Ave"
+};
+
+let udpatedEmployee = updateEmployeeWithKeyAndValue(employee, "streetAddress", "432 Oil St");
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = {...employee};
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
